fcdf6ec3c82571c303cc0ccc094f57ddf1268137
muditr <muditr97@gmail.com>
2017-02-27 03:24:10 +0530

    Update log.py

--- Question 1/log.py
+++ Question 1/log.py
@@ -1,5 +1,4 @@
 import logging
-
-def generator_log(write) :
-	logging.basicConfig(filename='log_text.log',filemode='w',datefmt='%d/%m/%Y %I:%M:%S %p',format='%(asctime)s %(name)-6s %(levelname) s: %(message)s',level=logging.DEBUG)
+def generator_log(write):
+	logging.basicConfig(filename='log_text.log', filemode='w', datefmt='%d/%m/%Y %I:%M:%S %p', format='%(asctime)s%(name)-6s%(levelname)s:%(message)s', level = logging.DEBUG)
 	logging.info(write)
